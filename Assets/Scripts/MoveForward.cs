a3fad8f31f7be4806d6550c917390c0de1b9a814
yayang12 <yayang1@s.sfusd.edu>
2023-02-02 12:43:32 -0800

    Started on Unit 2.2 #5
    5.Make animals into prefabs, finished rotating animal only

--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
+    public float speed = 40.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,6 @@ public class MoveForward : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
