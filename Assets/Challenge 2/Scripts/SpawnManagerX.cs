9d5ac1bc9edeb1fefb4b856f3e53d1375c174156
yayang12 <yayang1@s.sfusd.edu>
2023-02-07 11:06:18 -0800

    Challenge #8
    didn't finish 8 yet

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,22 +11,29 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval = Time.deltaTime;
+    private float spawnIntervalMin = 3;
+    private float spawnIntervalMax = 5;
+   
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomDog", startDelay, spawnInterval);
+        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        spawnInterval = Random.Range(spawnIntervalMin, spawnIntervalMax);
+        
     }
 
     // Spawn random ball at random x position at top of play area
-    void SpawnRandomDog ()
+    void SpawnRandomBall ()
     {
+        int BallIndex = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[BallIndex], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
